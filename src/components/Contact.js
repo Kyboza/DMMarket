253376836ecfc4b31ca8d1c57f197d096c75cbf9
Johan Clifford <johanclifford746@gmail.com>
2025-02-02 21:43:34 +0100

    added forgotten navigate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -56,7 +56,7 @@ const Contact = () => {
         confirmButtonText: "Ok",
         confirmButtonColor: "#14BFEEBF",
       });
-      navigate("/");
+      navigate("/DMMarket/");
     } catch (error) {
        await Swal.fire({
           title: "Error",
