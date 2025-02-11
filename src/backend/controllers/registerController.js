d19f5135cfbacda3fb4d1ac3b6d7370233a23e5f
Johan Clifford <johanclifford746@gmail.com>
2025-02-11 17:24:47 +0100

    test10

--- src/backend/controllers/registerController.js
+++ src/backend/controllers/registerController.js
@@ -1,50 +1,57 @@
 require("dotenv").config();
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");  // Importera Mongoose-modellen för användare
+const bcrypt = require("bcrypt");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const usersDB = {
+  users: require("../userData/users.json"),
+  setUsers: (data) => {
+    usersDB.users = data;
+  },
+};
 
 const handleNewUser = async (req, res) => {
   const { user, pwd, email } = req.body;
 
-  // Kolla att alla nödvändiga data finns med
   if (!user || !pwd || !email) {
     return res
       .status(400)
       .json({ message: "Username, password, and email are required" });
   }
 
+  const duplicateUser = usersDB.users.find(
+    (person) => person.username === user
+  );
+  const duplicateEmail = usersDB.users.find((person) => person.email === email);
+
+  if (duplicateUser) {
+    return res.sendStatus(409);
+  }
+
+  if (duplicateEmail) {
+    return res.status(409).json({ message: "Email already in use" });
+  }
+
   try {
-    // Kolla om användarnamnet redan finns
-    const duplicateUser = await User.findOne({ username: user });
-
-    if (duplicateUser) {
-      return res.status(409).json({ message: "Username already in use" });
-    }
-
-    // Kolla om e-postadressen redan används
-    const duplicateEmail = await User.findOne({ email });
-
-    if (duplicateEmail) {
-      return res.status(409).json({ message: "Email already in use" });
-    }
-
-    // Hasha lösenordet innan vi sparar användaren
     const hashedPwd = await bcrypt.hash(pwd, 10);
 
-    // Skapa en ny användare
-    const newUser = new User({
+    const newUser = {
       username: user,
       email: email,
-      roles: { User: 2001 }, // Default user role
+      roles: { User: 2001 },
       password: hashedPwd,
-    });
+    };
 
-    // Spara användaren i databasen
-    await newUser.save();
+    usersDB.setUsers([...usersDB.users, newUser]);
 
-    // Skicka tillbaka ett svar med successmeddelande
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "userData", "users.json"),
+      JSON.stringify(usersDB.users, null, 2)
+    );
+
+    console.log(usersDB.users);
     res.status(201).json({ success: `New user ${user} created!` });
   } catch (err) {
-    console.error(err);
     res.status(500).json({ message: err.message });
   }
 };
