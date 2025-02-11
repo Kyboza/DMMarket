afd14c61db859f4404da3fd2fdc1a38a39adff2d
Johan Clifford <johanclifford746@gmail.com>
2025-02-11 17:22:45 +0100

    test6

--- src/backend/controllers/registerController.js
+++ src/backend/controllers/registerController.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");  // Importera Mongoose-modellen för användare
 
 const handleNewUser = async (req, res) => {
